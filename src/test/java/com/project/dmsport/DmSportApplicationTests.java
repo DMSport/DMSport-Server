207ea217f7d9d26d56e278a871424bde2dd2b9fb
rlaisqls <rlaisqls@gmail.com>
2022-09-28 23:00:46 +0900

    ⚙️ :: 프로젝트 init

---
+++ src/test/java/com/project/dmsport/DmSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dmsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmSportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
