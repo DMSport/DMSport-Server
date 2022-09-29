345da53dc9aef63e6f16ec2093617e171748b012
rlaisqls <rlaisqls@gmail.com>
2022-09-29 13:52:12 +0900

    📑 :: UserFacade

---
+++ src/main/java/com/project/dmsport/domain/user/facade/UserFacade.java
@@ -0,0 +1,43 @@
+package com.project.dmsport.domain.user.facade;
+
+import com.project.dmsport.domain.user.domain.User;
+import com.project.dmsport.domain.user.domain.repository.UserRepository;
+import com.project.dmsport.domain.user.exception.PasswordMismatchException;
+import com.project.dmsport.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUserByEmail(email);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public boolean emailIsExist(String email) {
+        return userRepository.findByEmail(email).isPresent();
+    }
+
+    public void checkPassword(User user, String password) {
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw PasswordMismatchException.EXCEPTION;
+        }
+    }
+}
