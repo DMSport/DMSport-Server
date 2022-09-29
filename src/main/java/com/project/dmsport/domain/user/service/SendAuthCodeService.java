2436d2d22892bc924a1057b7d50bdca8f7055645
rlaisqls <rlaisqls@gmail.com>
2022-09-29 13:51:56 +0900

    📑 :: SendAuthCodeService
    나중에 비번찾기 할 때 필요한 이메일 인증

---
+++ src/main/java/com/project/dmsport/domain/user/service/SendAuthCodeService.java
@@ -0,0 +1,31 @@
+package com.project.dmsport.domain.user.service;
+
+import com.project.dmsport.domain.user.exception.UserNotFoundException;
+import com.project.dmsport.domain.user.facade.AuthCodeFacade;
+import com.project.dmsport.domain.user.facade.UserFacade;
+import com.project.dmsport.domain.user.presentation.dto.request.SendAuthCodeRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@Service
+public class SendAuthCodeService {
+
+    private final AuthCodeFacade authCodeFacade;
+    private final UserFacade userFacade;
+
+    public void execute(@Valid SendAuthCodeRequest request) {
+
+        String email = request.getEmail();
+
+        authCodeFacade.checkEmailDomain(email);
+
+        if (!userFacade.emailIsExist(email)) {
+            throw UserNotFoundException.EXCEPTION;
+        }
+
+        authCodeFacade.sendMail(email);
+    }
+}
