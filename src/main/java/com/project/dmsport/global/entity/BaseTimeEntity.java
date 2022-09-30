ec846ca982f9e383be1791774982eb6c77fcb96b
이하성 <tedsoftj1123@gmail.com>
2022-09-30 22:22:51 +0900

    ⚡️:: BaseTimeEntity수정

--- src/main/java/com/project/dmsport/global/entity/BaseTimeEntity.java
+++ src/main/java/com/project/dmsport/global/entity/BaseTimeEntity.java
@@ -3,6 +3,7 @@ package com.project.dmsport.global.entity;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
@@ -10,5 +11,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseTimeEntity {
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate = LocalDateTime.now();
 }
