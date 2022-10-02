738c668d22163141f31fb579219d84549aa42f14
KangMin <alsdl0629@dsm.hs.kr>
2022-10-03 02:47:19 +0900

    📑 :: Vote

---
+++ src/main/java/com/project/dmsport/domain/vote/domain/Vote.java
@@ -0,0 +1,50 @@
+package com.project.dmsport.domain.vote.domain;
+
+import com.project.dmsport.domain.vote.domain.enums.VoteType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@DynamicUpdate
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Vote {
+
+    @Id
+    @Column(name = "vote_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private VoteType voteType;
+
+    @NotNull
+    private Byte count;
+
+    @NotNull
+    private LocalDate voteDate;
+
+    @NotNull
+    private boolean complete;
+
+    @OneToMany(mappedBy = "vote")
+    private List<VoteUser> voteUsers;
+
+    @Builder
+    public Vote(VoteType voteType, Byte count, boolean complete) {
+        this.voteType = voteType;
+        this.count = count;
+        this.voteDate = LocalDate.now();
+        this.complete = complete;
+    }
+
+}
