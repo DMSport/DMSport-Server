a6f77e7641cf03189dcbfaeeff85a54e1d4e0170
KangMin <alsdl0629@dsm.hs.kr>
2022-10-03 14:03:28 +0900

    ⚡️ :: CrudRepo -> JpaRepo

--- src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteRepository.java
+++ src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteRepository.java
@@ -1,7 +1,7 @@
 package com.project.dmsport.domain.vote.domain.repository;
 
 import com.project.dmsport.domain.vote.domain.Vote;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VoteRepository extends CrudRepository<Vote, Long> {
+public interface VoteRepository extends JpaRepository<Vote, Long> {
 }
