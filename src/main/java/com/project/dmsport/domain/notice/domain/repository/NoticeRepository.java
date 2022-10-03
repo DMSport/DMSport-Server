0e861f16931539649faf9cbcbc42711604802635
이하성 <tedsoftj1123@gmail.com>
2022-10-03 15:04:38 +0900

    📑 :: 작성일별 내림차순 정렬

--- src/main/java/com/project/dmsport/domain/notice/domain/repository/NoticeRepository.java
+++ src/main/java/com/project/dmsport/domain/notice/domain/repository/NoticeRepository.java
@@ -3,5 +3,8 @@ package com.project.dmsport.domain.notice.domain.repository;
 import com.project.dmsport.domain.notice.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    List<Notice> findAllByOrderByCreatedDateDesc();
 }
