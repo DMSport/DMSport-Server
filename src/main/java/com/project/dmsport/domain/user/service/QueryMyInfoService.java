288e1651401d8e8533127007a7b01a5801708bcd
이하성 <tedsoftj1123@gmail.com>
2022-10-04 23:17:08 +0900

    ⚡️ :: transational

--- src/main/java/com/project/dmsport/domain/user/service/QueryMyInfoService.java
+++ src/main/java/com/project/dmsport/domain/user/service/QueryMyInfoService.java
@@ -5,12 +5,14 @@ import com.project.dmsport.domain.user.facade.UserFacade;
 import com.project.dmsport.domain.user.presentation.dto.response.QueryMyInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class QueryMyInfoService {
     private final UserFacade userFacade;
 
+    @Transactional(readOnly = true)
     public QueryMyInfoResponse execute() {
         User user = userFacade.getCurrentUser();
         return QueryMyInfoResponse.builder()
