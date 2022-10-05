4884ae3d32fa45b2fcdbbe6bb65a96ecb362c162
이하성 <tedsoftj1123@gmail.com>
2022-10-05 11:54:44 +0900

    ⚡️ :: 이름 변경^^

--- src/main/java/com/project/dmsport/domain/notice/exception/NoAuthorityException.java
+++ src/main/java/com/project/dmsport/domain/notice/exception/NoAuthorityException.java
@@ -4,7 +4,7 @@ import com.project.dmsport.global.error.exception.BusinessException;
 import com.project.dmsport.global.error.exception.ErrorCode;
 
 public class NoAuthorityException extends BusinessException {
-    public static final BusinessException EXCEPTION = new NoticeNotFoundException();
+    public static final BusinessException EXCEPTION = new NoAuthorityException();
     private NoAuthorityException() {
         super(ErrorCode.NO_AUTHORITY);
     }
