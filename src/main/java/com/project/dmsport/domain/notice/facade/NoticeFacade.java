abf52125eff55bde1be12b13db99810b0308ac11
KangMin <alsdl0629@dsm.hs.kr>
2022-10-06 23:15:59 +0900

    ♻️ :: 필요 없는 코드 삭제

--- src/main/java/com/project/dmsport/domain/notice/facade/NoticeFacade.java
+++ src/main/java/com/project/dmsport/domain/notice/facade/NoticeFacade.java
@@ -1,13 +1,8 @@
 package com.project.dmsport.domain.notice.facade;
 
-import com.project.dmsport.domain.club.domain.repository.ClubManagerRepository;
 import com.project.dmsport.domain.notice.domain.Notice;
-import com.project.dmsport.domain.notice.domain.enums.NoticeType;
 import com.project.dmsport.domain.notice.domain.repository.NoticeRepository;
 import com.project.dmsport.domain.notice.exception.NoticeNotFoundException;
-import com.project.dmsport.domain.user.domain.User;
-import com.project.dmsport.domain.user.domain.enums.Authority;
-import com.project.dmsport.global.exception.ForbiddenException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,15 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class NoticeFacade {
     private final NoticeRepository noticeRepository;
-    private final ClubManagerRepository clubManagerRepository;
+
     public Notice findNoticeById(Long id) {
         return noticeRepository.findById(id)
                 .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
     }
 
-    public void validateUser(User user, NoticeType type) {
-        if(user.getAuthority().equals(Authority.ADMIN)){
-            return;
-        }
-    }
 }
