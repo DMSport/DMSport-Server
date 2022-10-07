fff43174830564fe3d952e9e0c3dd8aa2826639b
KangMin <alsdl0629@dsm.hs.kr>
2022-10-07 16:24:47 +0900

    📑 :: 클럽장 변경 API

--- src/main/java/com/project/dmsport/domain/admin/presentation/AdminController.java
+++ src/main/java/com/project/dmsport/domain/admin/presentation/AdminController.java
@@ -1,10 +1,14 @@
 package com.project.dmsport.domain.admin.presentation;
 
+import com.project.dmsport.domain.admin.presentation.dto.request.ChangeClubManagerRequest;
 import com.project.dmsport.domain.admin.presentation.dto.request.ClubBanRequest;
 import com.project.dmsport.domain.admin.presentation.dto.response.ClubBanResponse;
 import com.project.dmsport.domain.admin.presentation.dto.response.UserListResponse;
+import com.project.dmsport.domain.admin.service.ChangeClubManagerService;
 import com.project.dmsport.domain.admin.service.ClubBanService;
+import com.project.dmsport.domain.admin.service.QueryUserListService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,15 +20,24 @@ import java.util.List;
 public class AdminController {
 
     private final ClubBanService clubBanService;
+    private final ChangeClubManagerService changeClubManagerService;
+    private final QueryUserListService queryUserListService;
 
     @PatchMapping("/ban")
     public ClubBanResponse clubBan(@RequestBody @Valid ClubBanRequest request) {
         return clubBanService.execute(request);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/users/manager/{user-id}")
+    public void changeClubManager(@PathVariable(name = "user-id") Long userId,
+                                  @RequestBody @Valid ChangeClubManagerRequest request) {
+        changeClubManagerService.execute(request, userId);
+    }
+
     @GetMapping("/users")
     public List<UserListResponse> searchAllUsers() {
-        return searchAllUsersService.execute();
+        return queryUserListService.execute();
     }
 
 }
