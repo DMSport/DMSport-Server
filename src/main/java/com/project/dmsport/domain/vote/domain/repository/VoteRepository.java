f1c2b97685056930d070e88d44aba334ea81bbdc
Kangmin <ehfehf0629@gmail.com>
2022-10-20 16:45:41 +0900

    🐛 :: 깔끔하게 고침

--- src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteRepository.java
+++ src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteRepository.java
@@ -1,18 +1,13 @@
 package com.project.dmsport.domain.vote.domain.repository;
 
 import com.project.dmsport.domain.vote.domain.Vote;
+import com.project.dmsport.domain.vote.domain.enums.VoteType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
-    @Query("select v from Vote v where v.voteType = 'LUNCH' and v.complete = false")
-    List<Vote> findAllByVoteTypeLunchAndCompleteFalse();
-
-    @Query("select v from Vote v where v.voteType = 'DINNER' and v.complete = false")
-    List<Vote> findAllByVoteTypeDinnerAndCompleteFalse();
-
+    List<Vote> findAllByVoteTypeAndCompleteFalse(VoteType voteType);
 
 }
--- src/main/java/com/project/dmsport/domain/vote/scheduler/CloseVoteScheduler.java
+++ src/main/java/com/project/dmsport/domain/vote/scheduler/CloseVoteScheduler.java
@@ -1,6 +1,7 @@
 package com.project.dmsport.domain.vote.scheduler;
 
 import com.project.dmsport.domain.vote.domain.Vote;
+import com.project.dmsport.domain.vote.domain.enums.VoteType;
 import com.project.dmsport.domain.vote.domain.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,7 +18,7 @@ public class CloseVoteScheduler {
     @Scheduled(cron = "0 50 12 * * MON-FRI")
     public void closeLunchVote() {
 
-        voteRepository.findAllByVoteTypeLunchAndCompleteFalse()
+        voteRepository.findAllByVoteTypeAndCompleteFalse(VoteType.LUNCH)
                 .stream()
                 .map(Vote::closeVote)
                 .map(voteRepository::save)
@@ -27,7 +28,7 @@ public class CloseVoteScheduler {
     @Scheduled(cron = "0 50 17 * * MON-FRI")
     public void closeDinnerVote() {
 
-        voteRepository.findAllByVoteTypeDinnerAndCompleteFalse()
+        voteRepository.findAllByVoteTypeAndCompleteFalse(VoteType.DINNER)
                 .stream()
                 .map(Vote::closeVote)
                 .map(voteRepository::save)
