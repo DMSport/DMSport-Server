649ee9d91ca932b1b681f5b856bdfa3570736bc2
이하성 <tedsoftj1123@gmail.com>
2022-10-22 23:28:08 +0900

    ⚡️ :: 공지 수정

--- src/main/java/com/project/dmsport/domain/notice/presentation/NoticeController.java
+++ src/main/java/com/project/dmsport/domain/notice/presentation/NoticeController.java
@@ -2,14 +2,18 @@ package com.project.dmsport.domain.notice.presentation;
 
 import com.project.dmsport.domain.notice.domain.enums.NoticeType;
 import com.project.dmsport.domain.notice.presentation.dto.request.CreateNoticeRequest;
+import com.project.dmsport.domain.notice.presentation.dto.request.ModifyNoticeRequest;
 import com.project.dmsport.domain.notice.presentation.dto.response.QueryAllNoticesResponse;
 import com.project.dmsport.domain.notice.presentation.dto.response.QueryNoticeDetailResponse;
 import com.project.dmsport.domain.notice.service.CreateNoticeService;
+import com.project.dmsport.domain.notice.service.ModifyNoticeService;
 import com.project.dmsport.domain.notice.service.QueryAllNoticesService;
 import com.project.dmsport.domain.notice.service.QueryNoticeDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/notices")
@@ -18,25 +22,32 @@ public class NoticeController {
     private final QueryAllNoticesService queryAllNoticesService;
     private final QueryNoticeDetailService queryNoticeDetailService;
     private final CreateNoticeService createNoticeService;
+    private final ModifyNoticeService modifyNoticeService;
     @GetMapping
-    public QueryAllNoticesResponse searchAllNotices() {
+    public QueryAllNoticesResponse searchNoticeList() {
         return queryAllNoticesService.execute();
     }
 
     @GetMapping("/{notice-id}")
-    public QueryNoticeDetailResponse searchNotice(@PathVariable("notice-id") Long noticeId) {
+    public QueryNoticeDetailResponse searchNoticeDetail(@PathVariable("notice-id") Long noticeId) {
         return queryNoticeDetailService.execute(noticeId);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public void createNotice(@RequestParam NoticeType type, @RequestBody CreateNoticeRequest request) {
-        createNoticeService.execute(request, type);
+    @PostMapping("/all")
+    public void createAdminNotice(@Valid @RequestBody CreateNoticeRequest request) {
+        createNoticeService.execute(request);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{notice-id}")
-    public void modifyNotice(@RequestParam("notice-id") Long noticeId) {
+    public void modifyNotice(@PathVariable("notice-id") Long noticeId, @Valid @RequestBody ModifyNoticeRequest request) {
+        modifyNoticeService.execute(request, noticeId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{notice-id}")
+    public void deleteNotice(@PathVariable("notice-id") Long noticeId) {
 
     }
 }
--- src/main/java/com/project/dmsport/domain/notice/service/ModifyNoticeService.java
+++ src/main/java/com/project/dmsport/domain/notice/service/ModifyNoticeService.java
@@ -1,8 +1,11 @@
 package com.project.dmsport.domain.notice.service;
 
 import com.project.dmsport.domain.notice.domain.Notice;
+import com.project.dmsport.domain.notice.exception.NoAuthorityException;
 import com.project.dmsport.domain.notice.facade.NoticeFacade;
 import com.project.dmsport.domain.notice.presentation.dto.request.ModifyNoticeRequest;
+import com.project.dmsport.domain.user.domain.User;
+import com.project.dmsport.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,13 +14,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ModifyNoticeService {
     private final NoticeFacade noticeFacade;
+    private final UserFacade userFacade;
 
     @Transactional
     public void execute(ModifyNoticeRequest request, Long noticeId) {
-
+        User user = userFacade.getCurrentUser();
+        Notice notice = noticeFacade.findNoticeById(noticeId);
+        if(!user.getEmail().equals(notice.getUser().getEmail())) {
+            throw NoAuthorityException.EXCEPTION;
+        }
         String title = request.getTitle();
         String content = request.getContent();
-        Notice notice = noticeFacade.findNoticeById(noticeId);
         notice.modifyNotice(title, content);
     }
 }
