2940fec4194958568ef9f01cc175ce17afd61272
Kangmin <ehfehf0629@gmail.com>
2022-10-24 23:12:09 +0900

    ⚡️ :: 오늘의 투표 리스트 가져오기 메소드 추가

--- src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteRepository.java
+++ src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteRepository.java
@@ -1,13 +1,17 @@
 package com.project.dmsport.domain.vote.domain.repository;
 
+import com.project.dmsport.domain.club.domain.enums.ClubType;
 import com.project.dmsport.domain.vote.domain.Vote;
 import com.project.dmsport.domain.vote.domain.enums.VoteType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
     List<Vote> findAllByVoteTypeAndCompleteFalse(VoteType voteType);
+
+    List<Vote> findAllByClubTypeAndVoteDateEquals(ClubType clubType, LocalDate date);
 
 }
