ae39e9bda679625e1f8db954a9b5d80678a07e46
Kangmin <ehfehf0629@gmail.com>
2022-10-26 21:10:20 +0900

    ⚡️ :: http 메소드 추가

--- src/main/java/com/project/dmsport/domain/TestController.java
+++ src/main/java/com/project/dmsport/domain/TestController.java
@@ -1,10 +1,12 @@
 package com.project.dmsport.domain;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
