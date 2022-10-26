28d0a1f2fea13d144cd79078e6fd395a3d347cfd
Kangmin <ehfehf0629@gmail.com>
2022-10-26 21:07:37 +0900

    📑 :: CI test

---
+++ src/main/java/com/project/dmsport/domain/TestController.java
@@ -0,0 +1,12 @@
+package com.project.dmsport.domain;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    public String hello() {
+        return "Hello World";
+    }
+
+}
