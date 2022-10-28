0bb9d5e5a568a15e71c8096c3fe989808d7e9e7b
rlaisqls <rlaisqls@gmail.com>
2022-10-28 17:45:12 +0900

    ⚡️ :: 비밀번호 찾기 permitAll

--- src/main/java/com/project/dmsport/global/security/SecurityConfig.java
+++ src/main/java/com/project/dmsport/global/security/SecurityConfig.java
@@ -53,6 +53,8 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .antMatchers(HttpMethod.POST, "/users/auth").permitAll()
                 .antMatchers(HttpMethod.PUT, "/users/auth").permitAll()
+                .antMatchers(HttpMethod.POST, "/users/password").permitAll()
+                .antMatchers(HttpMethod.POST, "/mail/find").permitAll()
 
                 .antMatchers(HttpMethod.POST, "/notices/club").hasAnyAuthority(
                         "BASKETBALL_MANAGER", "VOLLEYBALL_MANAGER", "BADMINTON_MANAGER", "SOCCER_MANAGER")
