0022649f217b438b70f4b9dd01511b7a0ef4624c
김은빈 <rlaisqls@gmail.com>
2022-10-30 20:15:14 +0900

    🐛 :: AllowedOriginPatterns

--- src/main/java/com/project/dmsport/global/web/WebMvcConfig.java
+++ src/main/java/com/project/dmsport/global/web/WebMvcConfig.java
@@ -11,8 +11,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
+                .allowCredentials(true)
                 .allowedHeaders("*");
     }
 }
