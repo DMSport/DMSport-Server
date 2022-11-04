c52a0888557822c35c513ed0a607eeb108886579
김은빈 <rlaisqls@gmail.com>
2022-11-04 20:11:23 +0900

    Update SecurityConfig.java

--- src/main/java/com/project/dmsport/global/security/SecurityConfig.java
+++ src/main/java/com/project/dmsport/global/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .antMatchers(HttpMethod.POST, "/users/auth").permitAll()
                 .antMatchers(HttpMethod.PUT, "/users/auth").permitAll()
-                .antMatchers(HttpMethod.POST, "/users/password").permitAll()
+                .antMatchers(HttpMethod.PUT, "/users/password").permitAll()
                 .antMatchers(HttpMethod.POST, "/users/mail/find").permitAll()
 
                 .antMatchers(HttpMethod.POST, "/notices/club").hasAnyAuthority(
