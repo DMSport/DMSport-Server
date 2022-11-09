44e9bea3ecb9982e388312b1f09ce56eb6cbef4c
rlaisqls <rlaisqls@gmail.com>
2022-11-09 12:38:24 +0900

    ⚡️ :: 랜덤 빈자리 채워넣기
    오늘에 해당하는 요일의 투표만 생성하도록 수정했고,
    메서드명도 좀 더 가독성있게 바꿔보았습니다.

---
+++ src/main/java/com/project/dmsport/domain/vote/scheduler/GenerateVoteScheduler.java
@@ -0,0 +1,88 @@
+package com.project.dmsport.domain.vote.scheduler;
+
+import com.project.dmsport.domain.club.domain.Club;
+import com.project.dmsport.domain.club.domain.ClubSchedule;
+import com.project.dmsport.domain.club.domain.enums.ActivityPlace;
+import com.project.dmsport.domain.club.domain.enums.ClubType;
+import com.project.dmsport.domain.club.domain.repository.ClubRepository;
+import com.project.dmsport.domain.club.domain.repository.ClubScheduleRepository;
+import com.project.dmsport.domain.vote.domain.Vote;
+import com.project.dmsport.domain.vote.domain.enums.VoteType;
+import com.project.dmsport.domain.vote.domain.repository.VoteRepository;
+import com.project.dmsport.global.util.RandomUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class GenerateVoteScheduler {
+
+    private final VoteRepository voteRepository;
+    private final ClubScheduleRepository clubScheduleRepository;
+    private final ClubRepository clubRepository;
+
+    @Scheduled(cron = "0 0 8 * * MON-FRI")
+    public void generateVote() {
+
+        DayOfWeek now = LocalDateTime.now().toLocalDate().getDayOfWeek();
+        List<ClubSchedule> clubScheduleList = clubScheduleRepository.findByDayOfWeek(now);
+
+        for (ActivityPlace activityPlace : ActivityPlace.values()) {
+
+            List<Club> activityClubList = clubRepository.findAll()
+                    .stream()
+                    .filter(c -> c.getPlace() == activityPlace)
+                    .map(Club::checkAndRestoreBan)
+                    .collect(Collectors.toList());
+
+            for (VoteType voteType : VoteType.values()) {
+
+                Optional<ClubSchedule> optionalClubSchedule = clubScheduleList
+                        .stream()
+                        .filter(cs -> cs.getVoteType() == voteType)
+                        .findFirst();
+
+                Club club;
+                if (optionalClubSchedule.isPresent()) {
+
+                    club = optionalClubSchedule.get().getClub();
+                    generateVote(club.getId(), VoteType.LUNCH);
+                    generateVote(club.getId(), VoteType.DINNER);
+
+                } else {
+
+                    club = getRandomHopingClub(activityClubList);
+                    generateVote(club.getId(), VoteType.LUNCH);
+
+                    club = getRandomHopingClub(activityClubList);
+                    generateVote(club.getId(), VoteType.DINNER);
+
+                }
+            }
+        }
+
+    }
+
+    private Club getRandomHopingClub(List<Club> activityClubList) {
+        return activityClubList
+                .stream()
+                .filter(Club::getHope)
+                .collect(Collectors.toList())
+                .get(RandomUtil.createRandomNumber(activityClubList.size()));
+    }
+
+    private void generateVote(ClubType clubType, VoteType voteType) {
+        voteRepository.save(Vote.builder()
+                .clubType(clubType)
+                .voteType(voteType)
+                .build());
+    }
+
+}
