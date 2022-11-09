e217f93c1a3e8b59760d7f5dcf8e6c6fbcff159a
rlaisqls <rlaisqls@gmail.com>
2022-11-09 12:46:06 +0900

    ♻️ :: CrudRepository

--- src/main/java/com/project/dmsport/domain/notice/domain/repository/NoticeRepository.java
+++ src/main/java/com/project/dmsport/domain/notice/domain/repository/NoticeRepository.java
@@ -1,12 +1,10 @@
 package com.project.dmsport.domain.notice.domain.repository;
 
 import com.project.dmsport.domain.notice.domain.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticeRepository extends CrudRepository<Notice, Long> {
     List<Notice> findAllByOrderByCreatedDateDesc();
 }
