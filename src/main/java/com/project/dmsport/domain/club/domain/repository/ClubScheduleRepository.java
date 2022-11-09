9219ab7be6741b42e78b46fa0d758b3eb12e50aa
rlaisqls <rlaisqls@gmail.com>
2022-11-09 12:39:38 +0900

    ⚡️ :: findByDayOfWeek

--- src/main/java/com/project/dmsport/domain/club/domain/repository/ClubScheduleRepository.java
+++ src/main/java/com/project/dmsport/domain/club/domain/repository/ClubScheduleRepository.java
@@ -4,12 +4,16 @@ package com.project.dmsport.domain.club.domain.repository;
 import com.project.dmsport.domain.club.domain.ClubSchedule;
 import com.project.dmsport.domain.club.domain.enums.ActivityPlace;
 import com.project.dmsport.domain.vote.domain.enums.VoteType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.DayOfWeek;
+import java.util.List;
 
 public interface ClubScheduleRepository extends CrudRepository<ClubSchedule, Long> {
 
     boolean existsByActivityPlaceAndDayOfWeekAndVoteType(ActivityPlace activityPlace, DayOfWeek dayOfWeek, VoteType voteType);
+
+    List<ClubSchedule> findByDayOfWeek(DayOfWeek dayOfWeek);
 
 }
