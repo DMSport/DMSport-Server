082e174d864e557d497e18049bb8ef456a45d5aa
Kangmin <ehfehf0629@gmail.com>
2022-11-13 15:34:52 +0900

    ⚡️ :: 투표한 유저 리스트 가져오기

--- src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteUserRepository.java
+++ src/main/java/com/project/dmsport/domain/vote/domain/repository/VoteUserRepository.java
@@ -4,12 +4,17 @@ import com.project.dmsport.domain.user.domain.User;
 import com.project.dmsport.domain.vote.domain.Vote;
 import com.project.dmsport.domain.vote.domain.VoteUser;
 import com.project.dmsport.domain.vote.domain.VoteUserId;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VoteUserRepository extends CrudRepository<VoteUser, VoteUserId> {
 
     Optional<VoteUser> findByVoteAndUser(Vote vote, User user);
+
+    @EntityGraph(attributePaths = {"user"})
+    List<VoteUser> findAllByVote(Vote vote);
 
 }
--- src/main/java/com/project/dmsport/domain/vote/service/QueryVoteHistoryListService.java
+++ src/main/java/com/project/dmsport/domain/vote/service/QueryVoteHistoryListService.java
@@ -7,12 +7,14 @@ import com.project.dmsport.domain.club.presentation.response.QueryVoteHistoryLis
 import com.project.dmsport.domain.club.presentation.response.QueryVoteHistoryListResponse.VoteResponse;
 import com.project.dmsport.domain.vote.domain.Vote;
 import com.project.dmsport.domain.vote.domain.repository.VoteRepository;
+import com.project.dmsport.domain.vote.domain.repository.VoteUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +22,7 @@ public class QueryVoteHistoryListService {
 
     private final ClubFacade clubFacade;
     private final VoteRepository voteRepository;
+    private final VoteUserRepository voteUserRepository;
 
     @Transactional(readOnly = true)
     public QueryVoteHistoryListResponse execute(ClubType clubType) {
@@ -31,13 +34,18 @@ public class QueryVoteHistoryListService {
         Map<LocalDate, List<VoteResponse>> voteStore = new HashMap<>();
 
         for (Vote vote: voteList) {
+            List<String> voteUserList = voteUserRepository.findAllByVote(vote)
+                    .stream()
+                    .map(voteUser -> voteUser.getUser().getName())
+                    .collect(Collectors.toList());
+
             if(!voteStore.containsKey(vote.getVoteDate())) {
                 voteStore.put(vote.getVoteDate(), new ArrayList<>(Collections.singletonList(
-                        QueryVoteHistoryListResponse.of(vote, club))
+                        QueryVoteHistoryListResponse.of(vote, club, voteUserList))
                 ));
             } else {
                 voteStore.get(vote.getVoteDate())
-                        .add(QueryVoteHistoryListResponse.of(vote, club));
+                        .add(QueryVoteHistoryListResponse.of(vote, club, voteUserList));
             }
         }
 
