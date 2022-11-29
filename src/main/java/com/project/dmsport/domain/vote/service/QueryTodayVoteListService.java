659f5f1d5ed0013a0c4479e5f83091e2209683f8
rlaisqls <rlaisqls@gmail.com>
2022-11-29 16:24:54 +0900

    ♻️ :: of 메서드

--- src/main/java/com/project/dmsport/domain/vote/service/QueryTodayVoteListService.java
+++ src/main/java/com/project/dmsport/domain/vote/service/QueryTodayVoteListService.java
@@ -3,9 +3,11 @@ package com.project.dmsport.domain.vote.service;
 import com.project.dmsport.domain.club.domain.Club;
 import com.project.dmsport.domain.club.domain.enums.ClubType;
 import com.project.dmsport.domain.club.facade.ClubFacade;
-import com.project.dmsport.domain.club.presentation.response.QueryTodayVoteListResponse;
-import com.project.dmsport.domain.club.presentation.response.QueryTodayVoteListResponse.VoteResponse;
+import com.project.dmsport.domain.vote.domain.repository.VoteUserRepository;
+import com.project.dmsport.domain.vote.presentation.dto.response.QueryTodayVoteListResponse;
 import com.project.dmsport.domain.vote.domain.repository.VoteRepository;
+import com.project.dmsport.domain.vote.presentation.dto.response.VoteResponse;
+import com.project.dmsport.domain.vote.presentation.dto.response.VoteUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +23,8 @@ public class QueryTodayVoteListService {
     private final ClubFacade clubFacade;
     private final VoteRepository voteRepository;
 
+    private final VoteUserRepository voteUserRepository;
+
     @Transactional(readOnly = true)
     public QueryTodayVoteListResponse execute(ClubType clubType, LocalDate date) {
 
@@ -28,20 +32,21 @@ public class QueryTodayVoteListService {
 
         List<VoteResponse> voteResponseList = voteRepository.findByClubTypeAndVoteDateEquals(clubType, date)
                 .stream()
-                .map(vote -> VoteResponse
-                        .builder()
-                        .voteId(vote.getId())
-                        .time(vote.getVoteType())
-                        .voteCount(vote.getCount())
-                        .build()
-                )
+                .map(vote -> {
+                    List<VoteUserResponse> voteUserList = voteUserRepository.findByVote(vote)
+                            .stream()
+                            .map(VoteUserResponse::of)
+                            .collect(Collectors.toList());
+
+                    return VoteResponse.of(vote, club, voteUserList);
+                })
                 .collect(Collectors.toList());
 
         return QueryTodayVoteListResponse.builder()
                 .isBan(club.getBan())
                 .banPeriod(club.getBanPeriod())
                 .maxPeople(club.getMaxPeople())
-                .vote(voteResponseList)
+                .voteList(voteResponseList)
                 .build();
     }
 
